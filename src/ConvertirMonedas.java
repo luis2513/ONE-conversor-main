7087db566d1600e93d25d518d1b80bf7b58d7afc
luis2513 <waltri.2513@hotmail.com>
2023-03-07 22:04:00 -0500

    conversor aggregadonv1

--- src/ConvertirMonedas.java
+++ src/ConvertirMonedas.java
@@ -3,25 +3,25 @@ import javax.swing.JOptionPane;
 public class ConvertirMonedas {
     public void ConvertirPesosColombianosADolar(double valorRecibido){
         double monedaDolar = valorRecibido / 4700.0;
-        monedaDolar = (double) Math.round(monedaDolar * 100d)/100;
+        monedaDolar = (double) Math.round(monedaDolar * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaDolar + " Dolares");
     }
 
     public void ConvertirPesosColombianosAEuro(double valorRecibido){
         double monedaEuro = valorRecibido / 5025.0;
-        monedaEuro = (double) Math.round(monedaEuro * 100d)/100;
+        monedaEuro = (double) Math.round(monedaEuro * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaEuro + " Euros");
     }
 
     public void ConvertirDolarAPesoscolombianos(double valorRecibido){
         double monedaPesos = valorRecibido / 0.000212;
-        monedaPesos = (double) Math.round(monedaPesos * 100d)/100;
+        monedaPesos = (double) Math.round(monedaPesos * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " Pesos");
     }
 
     public void ConvertirEuroAPesoscolombianos(double valorRecibido){
         double monedaPesos = valorRecibido / 0.00019;
-        monedaPesos = (double) Math.round(100d * monedaPesos)/100;
+        monedaPesos = (double) Math.round(100 * monedaPesos)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " Pesos");
     }
 
@@ -32,33 +32,33 @@ public class ConvertirMonedas {
     }
 
     public void ConvertirPesosColombianosAYen(double valorRecibido){
-        double monedaYen = valorRecibido / 0.029;
-        monedaYen = (double) Math.round(monedaYen * 100d)/100;
+        double monedaYen = valorRecibido / 34.67;
+        monedaYen = (double) Math.round(monedaYen * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaYen + " Yenes");
     }
 
     public void ConvertirLibrasEsterlinasAPesoscolombianos(double valorRecibido){
         double monedaPesos = valorRecibido / 0.00018;
-        monedaPesos = (double) Math.round(monedaPesos * 100d)/100;
+        monedaPesos = (double) Math.round(monedaPesos * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " Pesos");
     }
 
     public void ConvertirYenAPesoscolombianos(double valorRecibido){
-        double monedaPesos = valorRecibido / 34.57;
-        monedaPesos = (double) Math.round(monedaPesos * 100d)/100;
+        double monedaPesos = valorRecibido / 0.029;
+        monedaPesos = (double) Math.round(monedaPesos * 100)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " Pesos");
     }
 
     public void ConvertirWonAPesoscolombianos(double valorRecibido){
         double monedaWon = valorRecibido / 0.2762;
-        monedaWon = (double) Math.round(monedaWon * 100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaWon + " wones");
+        monedaWon = (double) Math.round(monedaWon * 100)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaWon + " pesos");
     }
 
     public void ConvertirPesoscolombianosAWon(double valorRecibido){
         double monedaPesos = valorRecibido / 3.62;
-        monedaPesos = (double) Math.round(monedaPesos * 100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " Pesos");
+        monedaPesos = (double) Math.round(monedaPesos * 100)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaPesos + " wones");
     }
 
 }
